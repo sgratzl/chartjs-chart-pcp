04ffe6cbedb28243e15981ed5464a12bd66c983d
Samuel Gratzl <sam@sgratzl.com>
2020-04-13 17:55:52 +0200

    add prettier

--- src/controllers/pcp.js
+++ src/controllers/pcp.js
@@ -2,12 +2,9 @@
 
 import * as Chart from 'chart.js';
 
-const defaults = {
-};
+const defaults = {};
 
 Chart.defaults.pcp = Chart.helpers.configMerge(Chart.defaults.global, defaults);
 
 const superClass = Chart.DatasetController.prototype;
-export const ParallelCoordinates = Chart.controllers.pcp = Chart.DatasetController.extend({
-
-});
+export const ParallelCoordinates = (Chart.controllers.pcp = Chart.DatasetController.extend({}));
